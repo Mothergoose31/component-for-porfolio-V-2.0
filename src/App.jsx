3eeeb669aa60fb6a42e1a5a30228dbbca9f365e1
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-14 14:13:15 -0800

    header overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import Nav from './components/Nav/Nav'
 import Three from './components/ThreeAnimation/Three'
 import './App.css';
 
@@ -7,8 +7,11 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        
-        <Three/>
+        <Nav/> 
+        <div id='container'>
+          <Three id='id'/>
+          <h1 id='header'>Joseph Beech</h1>
+        </div>
       </div>
     )
   }
