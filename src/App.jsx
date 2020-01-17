d7622c37091e23fa8ec5741e2d38a061ceb1f809
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-01-16 16:08:02 -0800

    react router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+
 import Nav from './components/Nav/Nav'
 import Three from './components/ThreeAnimation/Three'
-import './App.css';
+import About  from './components/AboutPage/About'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Nav/> 
-        <div id='container'>
-          <Three id='id'/>
-          <h1 id='header'>Joseph Beech</h1>
-        </div>
+      <div className = "App">
+        <Router>
+          <Nav/> 
+          
+            <div id='container'>
+            <Route  exact path ="/">
+              <Three id='id'/>
+              <h1 id='header'>Joseph Beech</h1>
+              </Route>
+            </div>
+          
+          <Route path ='/about'>
+            <About/>
+          </Route>
+        </Router>
       </div>
     )
   }
